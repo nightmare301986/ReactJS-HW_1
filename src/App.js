0425c964637cd792c2462f1497576b5a2e86e93d
Пахомов Евгений <nightmare23@yandex.ru>
2024-09-16 23:33:41 +0700

    Initial commit2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
+import './components/Message.css';
+import Message from './components/Message';
 import './App.css';
 
-function App() {
+const myText = 'Привет, мир!';
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        My First React App
+        <h3>Hello, {props.name}</h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Message text={myText} />
         </p>
         <a
           className="App-link"
@@ -15,6 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
           Learn React
         </a>
       </header>
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+
+function Message(props) {
+    return (
+
+        <h1 className="messageText">
+            {props.text}
+        </h1>
+    );
+
+}
+export default Message;
